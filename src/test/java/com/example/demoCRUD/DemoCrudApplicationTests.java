34a80d6832c18e8980bb22f99c3b6d0cfc926c03
oleh27yo <baibula.oo@gmail.com>
2023-05-03 13:41:25 +0300

    merged with addDTO

--- src/test/java/com/example/demoCRUD/DemoCrudApplicationTests.java
+++ src/test/java/com/example/demoCRUD/DemoCrudApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.demoCRUD;
 
+import com.example.demoCRUD.rest.PostRestController;
+import com.example.demoCRUD.rest.UserRestController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class DemoCrudApplicationTests {
 
+	@Autowired
+	UserRestController userRestController;
+	@Autowired
+	PostRestController postRestController;
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception{
+		assertThat(userRestController).isNotNull();
+		assertThat(postRestController).isNotNull();
 	}
-
 }
